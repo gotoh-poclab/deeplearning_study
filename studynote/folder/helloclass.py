f882a71cd132540a41d4c9fe00f26d777ec6fbeb
poclab <hgotoh0820@gmail.com>
2021-06-02 05:35:29 +0900

    week2

---
+++ studynote/folder/helloclass.py
@@ -0,0 +1,11 @@
+class Man:
+    def __init__(self, name):
+        self.name = name
+        print("Initialized!")
+
+    def hello(self):
+        print("Hello " + self.name + "!")
+
+    def goodby(self):
+        print("good-bye " + self.name + "!")
+
